70d7393d2e59a2578daa27047e2f48e47b3a2a44
Choi SeungHyoek <kwona6791@gmail.com>
2026-07-08 16:13:42 +0900

    2026 07 08 DI, IoC

---
+++ SpringCore/src/main/java/net/likelion/bebc25/oop/after/AfterTest.java
@@ -0,0 +1,9 @@
+package net.likelion.bebc25.oop.after;
+
+public class AfterTest {
+    void main(){
+        Car car = new HybridCar();
+        Driver driver = new Driver(car);
+        driver.driveCar();
+    }
+}
---
+++ SpringCore/src/main/java/net/likelion/bebc25/spring/AfterTest.java
@@ -0,0 +1,9 @@
+package net.likelion.bebc25.spring;
+
+public class AfterTest {
+    void main(){
+        Car car = new HybridCar();
+        Driver driver = new Driver(car);
+        driver.driveCar();
+    }
+}
